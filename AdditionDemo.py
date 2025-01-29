c69a71e6ebbe29fd0c8b305aae6c9c60a39ba004
Ebarnes83 <ericsolojedi@gmail.com>
2025-01-29 13:11:58 -0500

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,12 @@
+#Eric Barnes Python 01/29/2025
+number1 = 0
+number2 = 0
+
+print("Please enter 2 numbers")
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+sum = number1 + number2
+
+print(f"The sum of {number1} and {number2} is {sum}")
+
