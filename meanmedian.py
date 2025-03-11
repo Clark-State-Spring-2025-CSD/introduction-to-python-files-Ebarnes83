523f622f0f1b71c96f81484d54a519749b373fc2
Ebarnes83 <ericsolojedi@gmail.com>
2025-03-10 21:16:14 -0400

    Create meanmedian.py

---
+++ meanmedian.py
@@ -0,0 +1,31 @@
+#prompt the user to enter 5 numbers. Store them in a list.
+#Display the list as entered, small to large, and large to small
+#Calculate and display the mean and median
+#This is a guided practice. You can follow the video or your instructor will go
+#over this in class
+
+theList =[]
+
+for i in range(5):
+    theList.append(int(input(f"Number {i+1}:  ")))
+
+print("Orignal order: " + str(theList))
+
+theList.sort()
+
+print(f"small to large: {theList}")
+
+theList.sort(reverse=True)
+
+print(f"lagre to small: {theList}")
+
+median = theList[2]
+
+sum = 0
+
+for i in range(5):
+    sum += theList[i]
+
+    average = sum / 5
+
+print(f"The median is {median} and the mean is {average}")
