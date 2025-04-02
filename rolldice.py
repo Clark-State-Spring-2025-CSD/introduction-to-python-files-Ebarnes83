bbbebc551a09cdc889fe6df7c1d53a8cc652c8ac
Ebarnes83 <ericsolojedi@gmail.com>
2025-04-02 17:06:03 -0400

    Done

---
+++ rolldice.py
@@ -0,0 +1,42 @@
+#Write a function that takes as input number of dice and number of sides. The function will then return a list
+#of randomly generated numbers in the proper count and range. For example if the the function is asked to generate
+#3D6 or three sixed sided dice, then a potential output would be [2,2,6]
+#This function cannot have any input or print statements.
+#Write supporting code that will as the user for number of dice and sides, call the function, then report the results.
+#The function should not be called if number of dice is zero or less and instead should report bad input to the user
+#The function should not be called if number of sides is 1 or less and instead should report bad input to the user
+#Sample outputs (your text does not have to match exactly)
+
+# How many dice to roll? 3
+# How many sides? 6
+# Here are the results: [6, 1, 6]
+
+# How many dice to roll? 0
+# How many sides? 5
+# Error: Sides must be greater than 1 and dice count greater than 0.
+
+# How many dice to roll? 20
+# How many sides? 20
+# Here are the results: [18, 19, 6, 8, 13, 6, 6, 6, 18, 12, 20, 10, 14, 8, 14, 17, 12, 15, 20, 17]
+
+import random
+
+def roll_dice(num_dice, num_sides):
+    results = []
+    if num_dice <= 0:
+        return "You need more dice"
+    if num_sides <= 1:
+        return "You need more number of sides"
+    for _ in range(num_dice):
+        results.append(random.randint(1, num_sides))
+    return results
+
+num_dice = int(input("How many dice dice: "))
+num_sides = int(input("How many number of sides per die: "))
+
+dice_rolls = roll_dice(num_dice, num_sides)
+
+if dice_rolls == "You need more dice" or dice_rolls == "You need more number of sides":
+    print(dice_rolls)
+else:
+    print("Dice rolls:", dice_rolls)
